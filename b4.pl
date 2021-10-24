02acba3473e9b5b90d0ae8fadc6c44739d912f8c
jkpali <93093144+jkpali@users.noreply.github.com>
2021-10-24 10:34:55 -0700

    Create b4.pl

---
+++ b4.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+
+@m = ("A","E","W");
+@b = ("I","B","N","W","Z","E","A","G","S","D","K","O");
+@n = ("Φ","λ","O","E","D","Y","N","Δ");
+@alpha = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","S","R","T","U","V","W","X","Y","Z","θ","Φ","λ","τ","π","Δ");
+
+foreach $atrack (@m){
+        foreach $btrack (@alpha){
+                foreach $ctrack (@n){
+                        foreach $dtrack (@alpha){
+                                foreach $etrack (@alpha){
+                                        foreach $ftrack (@m){
+
+                                                print("$atrack$btrack$ctrack$dtrack$etrack$ftrack\n");
+
+                                        }
+                                }
+                        }
+                }
+        }
+}
