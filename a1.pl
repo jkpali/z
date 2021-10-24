b81f9bb0f64aad45f57a04e96fa508c87341938a
jkpali <93093144+jkpali@users.noreply.github.com>
2021-10-24 10:28:46 -0700

    Create a1.pl

---
+++ a1.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl
+
+@a = ("A","W","G","S","D","K","O");
+@b = ("I","B","N","W","Z","E");
+@n = ("Φ","λ","O","E","D","Y","N","Δ");
+@z = ("A","D");
+@k = ("A","S","K");
+@alpha = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","S
+","R","T","U","V","W","X","Y","Z","θ","Φ","λ","τ","π","Δ");
+
+
+foreach $atrack (@a){
+        foreach $btrack (@b){
+                foreach $ctrack (@n){
+                        foreach $dtrack (@z){
+                                foreach $etrack (@alpha){
+                                        foreach $ftrack (@k){
+
+                                        print("$atrack$btrack$ctrack$dtrack$etra
+ck$ftrack\n");
+
+                                        }
+                                }
+
+                        }
+
+                }
+
+        }
+
+}
