c39a3bf218571cdf741b2f85cb2674522dd9bca9
jkpali <93093144+jkpali@users.noreply.github.com>
2021-10-24 10:30:25 -0700

    Create a3.pl

---
+++ a3.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+@bk = ("I");
+@be = ("C");
+@a = ("A","W","G","S","D","K","O","H","M");
+@n = ("Φ","λ","O","E","D","Y","N","Δ");
+@z = ("A","D");
+@alpha = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","S
+","R","T","U","V","W","X","Y","Z","θ","Φ","λ","τ","π","Δ");
+
+
+foreach $atrack (@bk){
+        foreach $btrack (@alpha){
+                foreach $ctrack (@z){
+                        foreach $dtrack (@n){
+                                foreach $etrack (@be){
+                                        foreach $ftrack (@a){
+
+
+                                print("$atrack$btrack$ctrack$dtrack$etrack$ftrack\n");
+
+                                        }
+                                }
+                        }
+                }
+        }
+}
